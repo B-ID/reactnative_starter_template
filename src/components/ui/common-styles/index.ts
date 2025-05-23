e6e25dc04d559d20b072199fd2b34b53ce3511f2
b-id <idowu98official@gmail.com>
2025-05-23 06:17:03 +0100

    template setup

---
+++ src/components/ui/common-styles/index.ts
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export const commonStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    text: {
+        fontSize: 16,
+        color: '#333',
+    },
+    button: {
+        padding: 10,
+        backgroundColor: '#007BFF',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+    },
+});
